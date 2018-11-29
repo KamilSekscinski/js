9647edba8a0634ee21b742d881cf84a82b9c4f11
kamilsek <44327538+kamilsek@users.noreply.github.com>
2018-11-29 15:18:19 +0100

    Add files via upload
    zadania do samodzielnej pracy

---
+++ lab6/extra/main.js
@@ -0,0 +1,13 @@
+
+window.onload = function () {
+  let para = document.getElementsByTagName("p");
+//1. Znajdź paragraf zawierający najmniej tekstu i zmień kolor
+//  znaków na zielony. W paragrafie z największą ilością 
+//  tekstu zmień kolor na czerwony.
+for (let i=0; i<para.length; i++) {
+  const textFromP = para[i].textContent;
+  if ()
+}
+
+
+}
