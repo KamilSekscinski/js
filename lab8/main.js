1aaabdd9e493e4629937ad5185e2a7e53f6ca2de
kamilsek <44327538+kamilsek@users.noreply.github.com>
2018-12-11 23:25:10 +0100

    Add files via upload
    rozwiąz lab8

--- lab8/main.js
+++ lab8/main.js
@@ -1,14 +1,31 @@
-
-
-
 document.forms.form.onsubmit = function () {
     console.log(this.text.value);
     if (this.text.value) {
         const li2 = document.createElement("li");
+        
         li2.innerHTML = this.text.value;
+        
         document.getElementById('lista').appendChild(li2);
+        li2.dataset.status = "nie wykonano";
+        li2.dataset.dataUtw = Date ();
         //do edycji
-        // li2.contentEditable = true;
+        li2.contentEditable = true;
+
+
+        //button wykonane
+        const btWyk = document.createElement("button");
+        li2.appendChild(btWyk);
+        const btWykText = document.createTextNode('Wykonane');
+        btWyk.appendChild(btWykText);
+        btWyk.setAttribute("class", "wyk");
+
+        btWyk.onclick = function wyk() {
+            li2.dataset.status = "wykonano";    
+            li2.dataset.dataWyk = Date ();
+            this.style.backgroundColor = "green";   
+        }
+
+
         //button usuń
         const bt = document.createElement("button");
         li2.appendChild(bt);
@@ -21,20 +38,9 @@ document.forms.form.onsubmit = function () {
                 lista.removeChild(li2);
             }
         };
+        //czyszczenie pola onsubmit
+        // parent.form.text.value="";
 
-        //button wykonane
-        const btWyk = document.createElement("button");
-        li2.appendChild(btWyk);
-        const btWykText = document.createTextNode('Wykonane');
-        btWyk.appendChild(btWykText);
-        // btWyk.setAttribute("class", "wyk");
-
-        btWyk.onclick = function wyk() {
-             li2.dataset.status = "wykonano";
-            
-            this.style.backgroundColor = "green";
-            
-        }
     return false
     }
 
